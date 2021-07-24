4fc75a701b8683f475bdda8141e3fdbbadbf8085
manlol33633 <51886476+manlol33633@users.noreply.github.com>
2021-07-24 10:11:51 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+var form = document.createElement("form");
+
+var pet = document.createElement("label");
+pet.id = "pet";
+pet.innerHTML = "Farming Pet:";
+form.appendChild(pet);
+var petBox = document.createElement("input");
+petBox.id = "petbox";
+petBox.name = "petBox";
+form.appendChild(petBox);
+var lb = document.createElement("br");
+var lb2 = document.createElement("b ");
+
